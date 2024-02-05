f28d883bfd9cc04b6542bd691fc5ae22585533a0
Joff317 <j.freixanet@hotmail.com>
2024-02-05 16:00:42 +0100

    init back-end db connection and install needed dependencies

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+require("./config/db");
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "voici la réponse" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Running on : http://localhost:${process.env.PORT}`);
+});
