9cb6fe97f18f7738af4b3ae4ee2bdffdb94806b9
Joff317 <j.freixanet@hotmail.com>
2024-02-10 15:16:27 +0100

    init list model

---
+++ models/Lists.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const allowedGenre = [
+  "science-fiction",
+  "horror",
+  "action",
+  "drama",
+  "comedy",
+  "romance",
+  "thriller",
+  "fantasy",
+  "animation",
+  "documentary",
+  "other",
+];
+
+const listSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "text list is mandatory"],
+    minlength: [3, "Title list must be at least 3 characters long"],
+    maxlength: [70, "Title list must not exceed 200 characters"],
+  },
+
+  genre: {
+    type: String,
+    enum: {
+      values: allowedGenre,
+      message: `Invalid genre. Choose from: ${allowedGenre.join(", ")}`,
+    },
+  },
+
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  audioVisual: {
+    type: Schema.Types.ObjectId,
+    ref: "AudioVisual",
+  },
+});
+
+const List = model("List", listSchema);
+module.exports = List;
