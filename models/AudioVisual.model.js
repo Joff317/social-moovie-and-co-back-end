dd8d56083cf476a3760799e5e8b8d1d7e8e2dd73
Joff317 <j.freixanet@hotmail.com>
2024-02-10 12:19:32 +0100

    commentary model init and add to user and audiovisual model

--- models/AudioVisual.model.js
+++ models/AudioVisual.model.js
@@ -84,6 +84,13 @@ const audioVisualSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Commentary",
+    },
+  ],
 });
 
 const AudioVisual = model("AudioVisual", audioVisualSchema);
