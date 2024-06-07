2b5cbc9d836a6b09c5349cdb0ff589d56a17fd49
Joff317 <j.freixanet@hotmail.com>
2024-06-07 16:09:50 +0200

    cors 18

--- app.js
+++ app.js
@@ -10,29 +10,7 @@ require("./models/User.model");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CORS_URL }));
-
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-app.use(allowCors);
+app.use(cors({ origin: "https://front-final-project-orcin.vercel.app" }));
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -41,8 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "voici la réponse" });
 });
-
-app.options("*", cors());
 
 app.use("/api", require("./routes/index.routes"));
 
